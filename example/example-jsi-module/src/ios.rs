f2ef413272aa428a1d96b7aa988c815bdd911921
Laine Hallot <lainehallot@gmail.com>
2025-10-24 16:41:35 -0500

    remove example dir

--- example/example-jsi-module/src/ios.rs
+++
@@ -1,7 +0,0 @@
-#[no_mangle]
-pub extern "C" fn ExampleJsiModule_init(
-    rt: *mut jsi::sys::Runtime,
-    call_invoker: cxx::SharedPtr<jsi::sys::CallInvoker>,
-) {
-    crate::init(rt, call_invoker);
-}

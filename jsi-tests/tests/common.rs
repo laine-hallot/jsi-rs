1083be35d8228401565ae317a48df351c524d891
Laine Hallot <lainehallot@gmail.com>
2025-11-02 17:36:16 -0600

    refactor: remove obsolete test crate

--- jsi-tests/tests/common.rs
+++
@@ -1,9 +0,0 @@
-use cxx::UniquePtr;
-
-use jsi_tests::ffi::bridge::*;
-
-pub fn create_raw_runtime() -> UniquePtr<jsi_sys::Runtime> {
-    let config = create_runtime_config();
-    let rt = create_hermes_runtime(&*config);
-    cast_hermes_runtime(rt)
-}

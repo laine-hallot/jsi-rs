8b33efc5453eab4b10e7efa2e142998e040ff05f
Laine Hallot <lainehallot@gmail.com>
2025-11-02 18:22:44 -0600

    feat(jsi-sys): accept config options for hermes runtime

--- jsi-sys/include/helper.h
+++ jsi-sys/include/helper.h
@@ -9,9 +9,53 @@ std::unique_ptr<facebook::jsi::Runtime> cast_hermes_runtime(std::unique_ptr<face
   return runtime;
 }
 
-std::unique_ptr<hermes::vm::RuntimeConfig> create_runtime_config()
-{
-  return std::make_unique<hermes::vm::RuntimeConfig>();
+// This is not a binding for hermes::vm::RuntimeConfig,
+// passing a struct to create_runtime_config just seemed easier than individual arguments.
+struct CppHermesRuntimeConfig {
+  bool enable_eval;
+  bool verify_eval_ir;
+  bool optimized_eval;
+  bool async_break_check_in_eval;
+  bool es6_promise;
+  bool es6_proxy;
+  bool es6_class;
+  bool intl;
+  bool array_buffer;
+  bool microtask_queue;
+  bool enable_sampled_stats;
+  bool enable_sample_profiling;
+  bool randomize_memory_layout;
+  bool track_io;
+  bool enable_hermes_internal;
+  bool enable_hermes_internal_test_methods;
+  bool enable_generator;
+  bool enable_block_scoping;
+};
+
+std::unique_ptr<hermes::vm::RuntimeConfig> cpp_create_runtime_config(CppHermesRuntimeConfig& options)
+{ 
+  hermes::vm::RuntimeConfig::Builder cfgBuilder = hermes::vm::RuntimeConfig::Builder();
+  cfgBuilder.withEnableEval(options.enable_eval);
+  cfgBuilder.withVerifyEvalIR(options.verify_eval_ir);
+  cfgBuilder.withOptimizedEval(options.optimized_eval);
+  cfgBuilder.withAsyncBreakCheckInEval(options.async_break_check_in_eval);
+  cfgBuilder.withES6Promise(options.es6_promise);
+  cfgBuilder.withES6Proxy(options.es6_proxy);
+  cfgBuilder.withES6Class(options.es6_class);
+  cfgBuilder.withIntl(options.intl);
+  cfgBuilder.withArrayBuffer(options.array_buffer);
+  cfgBuilder.withMicrotaskQueue(options.microtask_queue);
+  cfgBuilder.withEnableSampledStats(options.enable_sampled_stats);
+  cfgBuilder.withEnableSampleProfiling(options.enable_sample_profiling);
+  cfgBuilder.withRandomizeMemoryLayout(options.randomize_memory_layout);
+  cfgBuilder.withTrackIO(options.track_io);
+  cfgBuilder.withEnableHermesInternal(options.enable_hermes_internal);
+  cfgBuilder.withEnableHermesInternalTestMethods(options.enable_hermes_internal_test_methods);
+  cfgBuilder.withEnableGenerator(options.enable_generator);
+  cfgBuilder.withEnableBlockScoping(options.enable_block_scoping);
+  
+  auto config = cfgBuilder.build();
+  return std::make_unique<hermes::vm::RuntimeConfig>(config);
 }
 
 std::unique_ptr<facebook::jsi::Value> eval_js(facebook::jsi::Runtime& rt, rust::Str js)
--- jsi-sys/src/ffi/hermes.rs
+++ jsi-sys/src/ffi/hermes.rs
@@ -1,3 +1,6 @@
+use cxx::{type_id, ExternType, UniquePtr};
+use std::pin::{pin, Pin};
+
 #[cxx::bridge]
 pub(crate) mod ffi {
     unsafe extern "C++" {
@@ -11,8 +14,12 @@ pub(crate) mod ffi {
         type JsiValue = crate::ffi::base::JsiValue;
 
         pub fn cast_hermes_runtime(ptr: UniquePtr<HermesRuntime>) -> UniquePtr<Runtime>;
-        pub fn create_runtime_config() -> UniquePtr<RuntimeConfig>;
         pub fn eval_js(rt: Pin<&mut Runtime>, js: &str) -> UniquePtr<JsiValue>;
+
+        type CppHermesRuntimeConfig = crate::hermes::HermesRuntimeConfig;
+        fn cpp_create_runtime_config(
+            config: Pin<&mut CppHermesRuntimeConfig>,
+        ) -> UniquePtr<RuntimeConfig>;
     }
 
     #[namespace = "hermes::vm"]
@@ -31,6 +38,64 @@ pub(crate) mod ffi {
         #[cxx_name = "makeHermesRuntime"]
         pub fn create_hermes_runtime(config: &RuntimeConfig) -> UniquePtr<HermesRuntime>;
     }
+}
+
+// This is not a binding for hermes::vm::RuntimeConfig,
+// passing a struct to create_runtime_config just seemed easier than individual arguments.
+pub struct HermesRuntimeConfig {
+    pub enable_eval: bool,
+    pub verify_eval_ir: bool,
+    pub optimized_eval: bool,
+    pub async_break_check_in_eval: bool,
+    pub es6_promise: bool,
+    pub es6_proxy: bool,
+    pub es6_class: bool,
+    pub intl: bool,
+    pub array_buffer: bool,
+    pub microtask_queue: bool,
+    pub enable_sampled_stats: bool,
+    pub enable_sample_profiling: bool,
+    pub randomize_memory_layout: bool,
+    pub track_io: bool,
+    pub enable_hermes_internal: bool,
+    pub enable_hermes_internal_test_methods: bool,
+    pub enable_generator: bool,
+    pub enable_block_scoping: bool,
+}
+
+unsafe impl ExternType for HermesRuntimeConfig {
+    type Id = type_id!("CppHermesRuntimeConfig");
+    type Kind = cxx::kind::Opaque;
+}
+
+impl Default for HermesRuntimeConfig {
+    fn default() -> Self {
+        Self {
+            enable_eval: true,
+            verify_eval_ir: false,
+            optimized_eval: false,
+            async_break_check_in_eval: true,
+            es6_promise: true,
+            es6_proxy: true,
+            es6_class: false,
+            intl: true,
+            array_buffer: true,
+            microtask_queue: false,
+            enable_sampled_stats: false,
+            enable_sample_profiling: false,
+            randomize_memory_layout: false,
+            track_io: false,
+            enable_hermes_internal: true,
+            enable_hermes_internal_test_methods: false,
+            enable_generator: true,
+            enable_block_scoping: false,
+        }
+    }
+}
+
+pub fn create_runtime_config(config: HermesRuntimeConfig) -> UniquePtr<RuntimeConfig> {
+    let cpp_config: Pin<&mut HermesRuntimeConfig> = pin!(config);
+    return cpp_create_runtime_config(cpp_config);
 }
 
 pub use ffi::*;
